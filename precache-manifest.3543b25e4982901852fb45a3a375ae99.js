09924872ca0bf7309031b44794bb0e6070961583
chanceDira <chancedesire60@gmail.com>
2021-12-18 15:53:44 +0200

    Updates

---
+++ precache-manifest.3543b25e4982901852fb45a3a375ae99.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d3832e94103f282f478578ddd6474f",
+    "url": "/Ck-Business-Ltd/index.html"
+  },
+  {
+    "revision": "f53758b9bff3c5f00add",
+    "url": "/Ck-Business-Ltd/static/css/14.5b91116b.chunk.css"
+  },
+  {
+    "revision": "46c1cf4c1515ac765e1d",
+    "url": "/Ck-Business-Ltd/static/css/main.0849d239.chunk.css"
+  },
+  {
+    "revision": "5c701f9e3afede3f7685",
+    "url": "/Ck-Business-Ltd/static/js/0.77a35a8b.chunk.js"
+  },
+  {
+    "revision": "5d5a7472ddb3a9e10811a76a172a8e88",
+    "url": "/Ck-Business-Ltd/static/js/0.77a35a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19517a0768ccd0b7efd6",
+    "url": "/Ck-Business-Ltd/static/js/1.957daa3d.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/Ck-Business-Ltd/static/js/1.957daa3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912a31eff7c9cfa50511",
+    "url": "/Ck-Business-Ltd/static/js/10.5a25ad10.chunk.js"
+  },
+  {
+    "revision": "b6632312a3be953da62107b37d979bc6",
+    "url": "/Ck-Business-Ltd/static/js/10.5a25ad10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a652b18858f1e6978d",
+    "url": "/Ck-Business-Ltd/static/js/11.a7c2b601.chunk.js"
+  },
+  {
+    "revision": "97636669c8193ace9a3b4f1548fc4d0b",
+    "url": "/Ck-Business-Ltd/static/js/11.a7c2b601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4f428a6f8ee59662fa",
+    "url": "/Ck-Business-Ltd/static/js/12.fb9e5672.chunk.js"
+  },
+  {
+    "revision": "1fc6541cf35d7e6d383e",
+    "url": "/Ck-Business-Ltd/static/js/13.3333cb7f.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/Ck-Business-Ltd/static/js/13.3333cb7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53758b9bff3c5f00add",
+    "url": "/Ck-Business-Ltd/static/js/14.1be046d6.chunk.js"
+  },
+  {
+    "revision": "43cc2d3f2c6595f2b4cee93cbc46c6bb",
+    "url": "/Ck-Business-Ltd/static/js/14.1be046d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737c36282c2fc1d13ecc",
+    "url": "/Ck-Business-Ltd/static/js/15.938fca2a.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/Ck-Business-Ltd/static/js/15.938fca2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593af4a7fae604400366",
+    "url": "/Ck-Business-Ltd/static/js/16.6131727a.chunk.js"
+  },
+  {
+    "revision": "3cb49564ee97f95722a2",
+    "url": "/Ck-Business-Ltd/static/js/17.39c23c7c.chunk.js"
+  },
+  {
+    "revision": "a82f1b6bf3680cc3510d",
+    "url": "/Ck-Business-Ltd/static/js/18.8aa33331.chunk.js"
+  },
+  {
+    "revision": "7140f5677c06df209dba",
+    "url": "/Ck-Business-Ltd/static/js/19.1a409c50.chunk.js"
+  },
+  {
+    "revision": "fe268f3ed599590cf296",
+    "url": "/Ck-Business-Ltd/static/js/2.d1bc9e0f.chunk.js"
+  },
+  {
+    "revision": "5735008847451525374b1f222e4ab316",
+    "url": "/Ck-Business-Ltd/static/js/2.d1bc9e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f8436e864c9f0253d1",
+    "url": "/Ck-Business-Ltd/static/js/20.2f4b1556.chunk.js"
+  },
+  {
+    "revision": "00febd0a2b5ee8a3b80f",
+    "url": "/Ck-Business-Ltd/static/js/21.8fc81c72.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/Ck-Business-Ltd/static/js/21.8fc81c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdd2e359eeffa478b2f",
+    "url": "/Ck-Business-Ltd/static/js/22.ae99e7df.chunk.js"
+  },
+  {
+    "revision": "2675a88b79b9486163d8",
+    "url": "/Ck-Business-Ltd/static/js/23.8751716f.chunk.js"
+  },
+  {
+    "revision": "d0d47e0ee26290c5542d",
+    "url": "/Ck-Business-Ltd/static/js/24.0162343e.chunk.js"
+  },
+  {
+    "revision": "3e5621fe229738442b4b",
+    "url": "/Ck-Business-Ltd/static/js/25.d289bd32.chunk.js"
+  },
+  {
+    "revision": "bfe8997296932905255e",
+    "url": "/Ck-Business-Ltd/static/js/3.a5a0515b.chunk.js"
+  },
+  {
+    "revision": "fe9b8c75644c30cb6851",
+    "url": "/Ck-Business-Ltd/static/js/4.5a30da18.chunk.js"
+  },
+  {
+    "revision": "97cc16ab0b7aeda089e5",
+    "url": "/Ck-Business-Ltd/static/js/5.8be380b5.chunk.js"
+  },
+  {
+    "revision": "036b417062b9124560c6",
+    "url": "/Ck-Business-Ltd/static/js/6.142f10a0.chunk.js"
+  },
+  {
+    "revision": "525a60cbbea74baa905b",
+    "url": "/Ck-Business-Ltd/static/js/7.c6cac626.chunk.js"
+  },
+  {
+    "revision": "46c1cf4c1515ac765e1d",
+    "url": "/Ck-Business-Ltd/static/js/main.3920f8d7.chunk.js"
+  },
+  {
+    "revision": "00fad4055750bbc9d0be",
+    "url": "/Ck-Business-Ltd/static/js/runtime-main.e0ffbe8e.js"
+  },
+  {
+    "revision": "d1ba1f6ef5331e5476946efc65aae283",
+    "url": "/Ck-Business-Ltd/static/media/11.d1ba1f6e.jpeg"
+  },
+  {
+    "revision": "13501fc6df2d4c544d8f359ee550431e",
+    "url": "/Ck-Business-Ltd/static/media/12.13501fc6.jpeg"
+  },
+  {
+    "revision": "556264bc0a55b6e1f194eceb3953c158",
+    "url": "/Ck-Business-Ltd/static/media/21.556264bc.jpeg"
+  },
+  {
+    "revision": "5d6aad0c4211e2712667c515973e7f49",
+    "url": "/Ck-Business-Ltd/static/media/person.5d6aad0c.jpeg"
+  },
+  {
+    "revision": "e20aaa8e9edd04941fc1b569e807ae47",
+    "url": "/Ck-Business-Ltd/static/media/room-4779953.e20aaa8e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/landing/precache-manifest.c5cb3fd5eb3e9bfcc9dcdb0d3bdf0881.js"
+  "/Ck-Business-Ltd/precache-manifest.3543b25e4982901852fb45a3a375ae99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/landing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ck-Business-Ltd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
